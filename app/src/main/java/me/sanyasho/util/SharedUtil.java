2ca73b49789ebf3f8de1195068cb7152bb4b3170
SanyaSho <68691958+sanyasho@users.noreply.github.com>
2024-11-04 19:09:16 +0300

    app, scripts: add license headers to my own files

--- app/src/main/java/me/sanyasho/util/SharedUtil.java
+++ app/src/main/java/me/sanyasho/util/SharedUtil.java
@@ -1,3 +1,21 @@
+/*
+* tf2classic-android-launcher
+* Copyright (C) 2024  SanyaSho
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package me.sanyasho.util;
 
 import static android.content.Context.ACTIVITY_SERVICE;
@@ -13,7 +31,7 @@ public class SharedUtil
 {
 	public static String TAG = "SharedUtil";
 
-	public static void startBrowser(Context ctx, String url)
+	public static void startBrowser( Context ctx, String url )
 	{
 		Intent browserIntent = new Intent( Intent.ACTION_VIEW, Uri.parse( url ) );
 		ctx.startActivity( browserIntent );
